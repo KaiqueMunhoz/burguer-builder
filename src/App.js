391cefa67108003eb28bcd4d53d7f753f5cfa555
Kaique Munhoz <munhoz13km@gmail.com>
2019-01-25 14:43:16 -0200

    Feat: Use the new component Layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Layout from './components/Layout'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
+      <Layout>
+        <p>Teste</p>
+      </Layout>
     );
   }
 }
